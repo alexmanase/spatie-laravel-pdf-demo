3b4b1ead82bd28aef2b7e38d3983172ae5b9f5d2
Alex Manase <malexandru699@gmail.com>
2024-07-08 10:26:18 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\LaravelPdf\Facades\Pdf;
 
 Route::get('/', function () {
-    return view('welcome');
+     return Pdf::view('pdf.fresh')
+            ->withBrowsershot(function ($browsershot) {
+                if (!app()->isProduction()) {
+                    $browsershot
+                        ->setNodeBinary(
+                            "~/Library/Application\ Support/Herd/config/nvm/versions/node/v18.20.1/bin/node"
+                        )
+                        ->setNpmBinary(
+                            "~/Library/Application\ Support/Herd/config/nvm/versions/node/v18.20.1/bin/npm"
+                        );
+                }
+            })
+            ->format('a4')
+            ->footerView('pdf.fresh.footer');
 });
